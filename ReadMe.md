9803aeaf12b26e89a601987f067070e200432133
Pamtors <fammem@hotmail.com>
2026-03-23 20:23:15 +0100

    primer commit

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+Información básica.
---
+++ js/config.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAGOWJMlWWwqWW_M0AwYilhBIuBE5olWDg",
+    authDomain: "pro1-7115e.firebaseapp.com",
+    projectId: "pro1-7115e",
+    storageBucket: "pro1-7115e.firebasestorage.app",
+    messagingSenderId: "354009326153",
+    appId: "1:354009326153:web:3e7f4cae419b9cc1711598"
+});
+
+const auth = firebase.auth();

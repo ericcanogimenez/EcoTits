d8532e9cbb43e89270057f65eb222f8514b308e2
Eric <ericcanogimenez@gmail.com>
2026-03-23 20:02:51 +0100

    proyecto

---
+++ js/config.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyBlzJUaylP4tOSnkmA6vV1ucjm9lC-eNbk",
+  authDomain: "ecotits-9f3b9.firebaseapp.com",
+  projectId: "ecotits-9f3b9",
+  storageBucket: "ecotits-9f3b9.firebasestorage.app",
+  messagingSenderId: "1000612840037",
+  appId: "1:1000612840037:web:fb7536d621d10de5ea3c8c"
+});
+
+const auth = firebase.auth();

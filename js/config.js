d547a468317d4207c0020468bf9b4aaccd1c3048
oleguer47 <alu.oleguer.alberti@mataro.epiaedu.cat>
2026-03-24 17:47:53 +0100

    modificacion oleguer

---
+++ js/config.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAoQIe4t7aJ-h9R8Qe556ZaLujMkmwX5yg",
+  authDomain: "kdiustit.firebaseapp.com",
+  projectId: "kdiustit",
+  storageBucket: "kdiustit.firebasestorage.app",
+  messagingSenderId: "653828812571",
+  appId: "1:653828812571:web:7bc1b62dc7d1429ea42dbe"
+});
+
+const auth = firebase.auth();
